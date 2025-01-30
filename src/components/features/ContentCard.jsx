db008bc62b63ef6ea15dfad5ed8d6f3b533d8623
oxbryte <silascyrax@gmail.com>
2025-01-30 01:00:53 +0100

    Refactor ContentCard component and update styles

--- src/components/features/ContentCard.jsx
+++ src/components/features/ContentCard.jsx
@@ -1,4 +1,5 @@
 import { FiThumbsUp } from "react-icons/fi";
+import { HiOutlineCurrencyDollar } from "react-icons/hi";
 import { TbMessage2 } from "react-icons/tb";
 
 export default function ContentCard() {
@@ -22,22 +23,38 @@ export default function ContentCard() {
           </div>
           <div className="flex gap-2 items-center text-white/60">
             <TbMessage2 size={21} />
-            <p className="!m-0 text-sm text-white/90">30</p>
+            <p className="!m-0 text-sm text-white/80">30</p>
           </div>
         </div>
         <div className="flex items-center gap-4">
           <div className="flex flex-col items-center">
-            <p className="text-white/90 text-sm">Total Mints</p>
+            <p className="text-white/80 text-sm">Total Mints</p>
             <p className="text-white/60 text-sm">20/100</p>
           </div>
           <div className="flex flex-col items-center">
-            <p className="text-white/90 text-sm">Price</p>
+            <p className="text-white/80 text-sm">Price</p>
             <p className="text-white/60 text-sm">0.02ETH</p>
           </div>
           <button className="px-6 py-2.5 bg-blue-600 rounded-full text-sm">
             Mint
           </button>
         </div>
+      </div>
+      <div className="w-full flex items-center gap-4 justify-between">
+        <div className="flex gap-3 items-center">
+          <div className="w-12 h-12 bg-white/30 rounded-full"></div>
+          <div className="space-y-0">
+            <h1 className="font-semibold !m-0 text-lg text-white/80">
+              Full name
+            </h1>
+            <p className="text-white/60 !m-0 text-sm">@fullname</p>
+          </div>
+          <p className="text-xs text-white/60">20 hours ago</p>
+        </div>
+        <button className="border border-white/60 text-white/60 px-3 py-1.5 gap-2 rounded-full flex items-center">
+          <HiOutlineCurrencyDollar size={20} />
+          Tip
+        </button>
       </div>
     </div>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import ContentCard from "../components/features/ContentCard";
 
 export default function Home() {
   return (
-    <div className="w-full space-y-6 mx-auto">
+    <div className="w-full space-y-10 mx-auto">
       <ContentCard />
       <ContentCard />
       <ContentCard />
