bf4cc7d042544c94af89f5cf0b4dedb54610108f
oxbryte <silascyrax@gmail.com>
2025-02-12 22:07:16 +0100

    feat: Remove loading state handling from ProtectedRoute and adjust isAuthenticated check in useUser hook

--- src/ProtectedRoute.jsx
+++ src/ProtectedRoute.jsx
@@ -2,9 +2,9 @@ import { Navigate, Outlet } from "react-router-dom";
 import { useUser } from "./components/hooks/useUser";
 
 const ProtectedRoute = () => {
-  const { isAuthenticated, isLoading } = useUser();
+  const { isAuthenticated } = useUser();
 
-    if (isLoading) return <div>Loading...</div>;
+  // if (isLoading) return <div>Loading...</div>;
   return isAuthenticated ? <Outlet /> : <Navigate to="/login" replace />;
 };
 
